396338f3034e5c6d76e22a25e3386cf28ab2dd60
YosapornPet <yosaporn.pet@gmail.com>
2024-08-05 15:46:33 +0700

    Update BackendApplication.java

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -3,9 +3,11 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAutoConfiguration
 public class BackendApplication {
 
 	public static void main(String[] args) {
